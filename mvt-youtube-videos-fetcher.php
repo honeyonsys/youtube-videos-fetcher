45e7f7feb188c1dc746cfaf250b98ff1783e6d52
honeyonsys@gmail.com <honeyonsys@gmail.com>
2024-08-01 11:53:51 +0530

    changes done by sajjan on plugin

--- mvt-youtube-videos-fetcher.php
+++ mvt-youtube-videos-fetcher.php
@@ -1,9 +1,10 @@
 <?php
 /*
-Plugin Name: YouTube Sync Plugin
+Plugin Name: YouTube Sync MVT
 Description: Sync YouTube videos and thumbnails to WordPress database.
 Version: 1.0
-Author: Harish Kumar
+Author: Myvirtualteams
+Author URI: https://www.myvirtualteams.com/
 */
 
 // Hook to add admin menu
@@ -15,7 +16,7 @@ function youtube_sync_plugin_menu() {
     // Add top-level menu page
     add_menu_page(
         'YouTube List', // Page title
-        'MVT YouTube Sync', // Menu title
+        'YouTube Sync MVT', // Menu title
         'manage_options', // Capability
         'youtube-sync-plugin-list', // Menu slug
         'youtube_sync_plugin_list_page', // Function to display the list page
@@ -52,8 +53,10 @@ function youtube_sync_plugin_list_page() {
             <input type="hidden" name="youtube_sync_manual_sync" value="1" />
             <input type="submit" class="button button-primary" value="Sync Videos" />
         </form>
+       
         <div id="video-list"></div>
     </div>
+   
     <?php
 }
 
@@ -117,6 +120,8 @@ function youtube_sync_plugin_playlist_id_callback() {
 }
 
 function youtube_sync_plugin_fetch_videos() {
+
+
     $api_key = get_option('youtube_sync_plugin_api_key');
     $playlist_id = get_option('youtube_sync_plugin_playlist_id');
     $page_token = isset($_POST['page_token']) ? sanitize_text_field($_POST['page_token']) : '';
@@ -138,29 +143,37 @@ function youtube_sync_plugin_fetch_videos() {
     if (!isset($data->items)) {
         wp_send_json_error('No videos found');
     }
-
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'youtube_videos';
+  
     $videos = array();
-
+  
     foreach ($data->items as $item) {
         if (isset($item->snippet->resourceId->videoId)) {
             $video_id = $item->snippet->resourceId->videoId;
             $title = $item->snippet->title;
             $description = $item->snippet->description;
-            $thumbnail = $item->snippet->thumbnails->default->url;
+            $thumbnail = $item->snippet->thumbnails->maxres->url;
+            $channel = $item->snippet->channelTitle;
+            $playlistId = $item->snippet->playlistId;
             $publishedAt = $item->snippet->publishedAt;
+     
+            
 
-            // Check if the video already exists in the database
-            $existing_video = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM $table_name WHERE video_id = %s", $video_id));
+            // Check if a post with the same title already exists
+            $existing_post = get_page_by_title(sanitize_text_field($title), OBJECT, 'inkish-video');
+          
+            // $selected_category = get_post_categories_by_title($title);
 
             $videos[] = array(
                 'video_id' => $video_id,
                 'title' => $title,
                 'thumbnail' => $thumbnail,
                 'description' =>$description,
+                'channel' => $channel,
+                'playlistId' =>  $playlistId ,
                 'publishedAt' => $publishedAt,
-                'status' => $existing_video > 0 ? 'Imported' : 'Not Imported'
+                // 'categories' => $category_list ,
+                // 'selected_category' => $selected_category,
+                'status' =>  $existing_post ? 'Imported' : 'Not Imported'
             );
         }
     }
@@ -169,13 +182,13 @@ function youtube_sync_plugin_fetch_videos() {
         'videos' => $videos,
         'nextPageToken' => isset($data->nextPageToken) ? $data->nextPageToken : null,
         'totalResults' => $data->pageInfo->totalResults,
-        'resultsPerPage' => $data->pageInfo->resultsPerPage
+        'resultsPerPage' => $data->pageInfo->resultsPerPage,
+
     ));
 }
 
 function youtube_sync_plugin_import_videos() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'youtube_videos';
+
     $video_ids = isset($_POST['video_ids']) ? $_POST['video_ids'] : array();
     $videos = isset($_POST['videos']) ? $_POST['videos'] : array();
 
@@ -191,71 +204,53 @@ function youtube_sync_plugin_import_videos() {
             $datetime = new DateTime($iso8601_date);
             $formatted_date = $datetime->format('Y-m-d H:i:s');
 
-            // $wpdb->replace(
-            //     $table_name,
-            //     array(
-            //         'video_id' => $video['video_id'],
-            //         'title' => $video['title'],
-            //         'thumbnail' => $video['thumbnail'],
-            //         'description' => $video['description'],
-            //         'publishedAt' =>  $formatted_date
-            //     ),
-            //     array(
-            //         '%s',
-            //         '%s',
-            //         '%s',
-            //         '%s',
-            //         '%s'
-            //     )
-            // );
+            // Check if a post with the same title already exists
+            $existing_post = get_page_by_title(sanitize_text_field($video['title']), OBJECT, 'inkish-video');
 
-            $sql = $wpdb->prepare(
-                "
-                INSERT INTO $table_name (video_id, title, thumbnail, description, publishedAt)
-                VALUES (%s, %s, %s, %s, %s)
-                ON DUPLICATE KEY UPDATE
-                    title = VALUES(title),
-                    thumbnail = VALUES(thumbnail),
-                    description = VALUES(description),
-                    publishedAt = VALUES(publishedAt)
-                ",
-               $video['video_id'], $video['title'], $video['thumbnail'], $video['description'], $formatted_date
+            // Prepare the post data
+            $post_data = array(
+                'post_title'   => sanitize_text_field($video['title']),
+                'post_content' => sanitize_text_field($video['description']),
+                'post_status'  => 'publish',
+                'post_type'    => 'inkish-video',
             );
+
+            if ($existing_post) {
+                // Update the existing post
+                $post_data['ID'] = $existing_post->ID;
+                $post_id = wp_update_post($post_data);
+
+                // Update the custom fields
+                update_post_meta($post_id, 'yvtwp_feed_key', sanitize_text_field($video['playlistId']));
+                update_post_meta($post_id, 'yvtwp_video_key', sanitize_text_field($video['video_id']));
+               
+            } else {
+                // Create a new post
+                $post_id = wp_insert_post($post_data);
+
+                // Add custom fields
+                add_post_meta($post_id, 'yvtwp_feed_key', sanitize_text_field($video['playlistId']));
+                add_post_meta($post_id, 'yvtwp_video_key', sanitize_text_field($video['video_id']));
             
-            $wpdb->query($sql);
+            }
 
 
+            // Handle the thumbnail
+            $thumbnail_url = esc_url($video['thumbnail']);
+            $thumbnail_id = media_sideload_image($thumbnail_url, $post_id, null, 'id');
 
-
-
-
+            if (!is_wp_error($thumbnail_id)) {
+                set_post_thumbnail($post_id, $thumbnail_id);
+            }
+            //Update video lang in the post
+            set_the_video_lang_in_post($video['video_id'], $post_id);
         }
     }
 
     wp_send_json_success('Videos imported successfully');
 }
 
-function youtube_sync_plugin_create_table() {
-    global $wpdb;
-    $table_name = $wpdb->prefix . 'youtube_videos';
-    $charset_collate = $wpdb->get_charset_collate();
 
-    $sql = "CREATE TABLE $table_name (
-        id mediumint(9) NOT NULL AUTO_INCREMENT,
-        video_id varchar(50) NOT NULL,
-        title text NOT NULL,
-        thumbnail varchar(255) DEFAULT '' NOT NULL,
-        description text,
-        publishedAt datetime,
-        PRIMARY KEY  (id),
-        UNIQUE KEY video_id (video_id)
-    ) $charset_collate;";
-
-    require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
-    dbDelta($sql);
-}
-
-register_activation_hook(__FILE__, 'youtube_sync_plugin_create_table');
 
 // Enqueue scripts
 add_action('admin_enqueue_scripts', 'youtube_sync_plugin_enqueue_scripts');
@@ -265,136 +260,24 @@ function youtube_sync_plugin_enqueue_scripts($hook) {
         return;
     }
 
-    wp_enqueue_script('youtube-sync-plugin-script', plugin_dir_url(__FILE__) . 'youtube-sync-plugin.js', array('jquery'), null, true);
+    wp_enqueue_style( 'yt-style', plugin_dir_url(__FILE__)  . 'assets/css/style.css', array(), time(), 'all' );
+
+    wp_enqueue_script('youtube-sync-plugin-script', plugin_dir_url(__FILE__) . 'assets/js/youtube-sync-plugin.js', array('jquery'), null, true);
+    wp_enqueue_script('sweetalert2-script', 'https://cdn.jsdelivr.net/npm/sweetalert2@11.12.3/dist/sweetalert2.all.min.js', array('jquery'), null, true);
+    wp_enqueue_style('custom-sweetalert2-style', 'https://cdn.jsdelivr.net/npm/sweetalert2@11.12.3/dist/sweetalert2.min.css');                
+
+  
 
     wp_localize_script('youtube-sync-plugin-script', 'youtubeSyncPlugin', array(
         'ajax_url' => admin_url('admin-ajax.php')
     ));
 
-    // Add inline CSS
-    add_action('admin_head', function() {
-        echo '<style>
-            #loader-overlay {
-                position: fixed;
-                top: 0;
-                left: 0;
-                width: 100%;
-                height: 100%;
-                background: rgba(0, 0, 0, 0.5);
-                z-index: 1000;
-                display: flex;
-                align-items: center;
-                justify-content: center;
-            }
-
-            .loader {
-                border: 16px solid #f3f3f3;
-                border-radius: 50%;
-                border-top: 16px solid #3498db;
-                width: 120px;
-                height: 120px;
-                animation: spin 2s linear infinite;
-            }
-
-            @keyframes spin {
-                0% { transform: rotate(0deg); }
-                100% { transform: rotate(360deg); }
-            }
-            .pagination {
-                display: list-item;
-                justify-content: center;
-                margin: 20px 0;
-                padding: 10px;
-                width: 100%;
-                overflow-y: scroll;
-            }
-            .pagination a {
-                padding: 8px 12px;
-                margin: 0 4px;
-                border: 1px solid #ccc;
-                text-decoration: none;
-                color: #0073aa;
-                background-color: #f1f1f1;
-                border-radius: 3px;
-            }
-            .pagination a:hover {
-                background-color: #0073aa;
-                color: #fff;
-            }
-            .pagination a.current {
-                background-color: #0073aa;
-                color: #fff;
-                pointer-events: none;
-            }
-            .imported {
-                color: green;
-            }
-            .not-imported {
-                color: red;
-            }
-            #import-selected-videos {
-                float:right;
-                margin-bottom:5px;
-            }
-        </style>';
-    });
+   
 
 }
 
 
 
-function enqueue_my_scripts() {
-
-    wp_enqueue_style( 'yt-style', plugin_dir_url(__FILE__)  . 'assets/css/style.css', array(), time(), 'all' );
-    wp_enqueue_script('my-infinite-scroll',   plugin_dir_url(__FILE__) . 'assets/js/infinite-scroll.js', array('jquery'), null, true);
-    wp_enqueue_script('my-fontawesome',   'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css', null, true);
-
-    wp_localize_script('my-infinite-scroll', 'myAjax', array(
-        'ajaxurl' => admin_url('admin-ajax.php')
-    ));
-}
-add_action('wp_enqueue_scripts', 'enqueue_my_scripts');
-
-// Shortcode function to display initial data
-
-function display_video_data() {
-
-    ob_start();
- 
-    ?>
-       <div class="listing-sec view-all-outer" data-selected-cat="">
-           <div id="append_video_content" class="tiles-area filter_data_section "> </div>
-      </div>
-      <div class="loader-wrapper" style="display: none;"><div class="loader_inkish"><img src="https://inkish.tv/wp-content/plugins/the-preloader/images/preloader.GIF" alt="Loader"></div></div>
-    <?php
- 
-
-    return ob_get_clean();
-}
-
-
-// REST API endpoint to fetch additional data
-function fetch_videos_data() {
-    global $wpdb;
-
-    $page = isset($_GET['page']) ? intval($_GET['page']) : 1;
-    $per_page = 20;
-    $offset = ($page - 1) * $per_page;
-    $table_name = $wpdb->prefix . 'youtube_videos';
-    $query = $wpdb->prepare("SELECT * FROM $table_name ORDER BY publishedAt DESC LIMIT %d OFFSET %d", $per_page, $offset);
-    $results = $wpdb->get_results($query);
-
-    // Return results as JSON
-    wp_send_json($results);
-}
-add_action('wp_ajax_fetch_videos_data', 'fetch_videos_data');
-add_action('wp_ajax_nopriv_fetch_videos_data', 'fetch_videos_data');
-
-// Register the shortcode
-function register_video_shortcode() {
-    add_shortcode('video_data', 'display_video_data');
-}
-add_action('init', 'register_video_shortcode');
 
 
 
@@ -405,3 +288,44 @@ function truncate_description($text, $chars = 100) {
     $truncated = substr($text, 0, $chars) . '...';
     return $truncated;
 }
+
+
+function set_the_video_lang_in_post($video_id, $post_id) {
+    $api_key = get_option('youtube_sync_plugin_api_key');
+    
+    $post_data = get_post_meta($post_id, 'video_avail_lang', true);
+    if (empty($post_data)) {
+        $lang = '';
+        $url_caption = "https://www.googleapis.com/youtube/v3/captions?videoId=" . $video_id . "&key=" . $api_key . "&part=snippet";
+        $response = wp_remote_get($url_caption);
+        
+        if (is_wp_error($response)) {
+            // Handle error
+            return;
+        }
+
+        $body = wp_remote_retrieve_body($response);
+        $json = json_decode($body, true);
+        
+        if (!empty($json['items'])) {
+            foreach ($json['items'] as $value) {
+                $lang .= "<span>" . esc_html(ucwords($value['snippet']['language'])) . "</span>  ";
+            }
+            $lang = rtrim($lang, "  "); // Trim the trailing spaces
+            update_post_meta($post_id, 'video_avail_lang', $lang);
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
